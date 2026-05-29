674f525f7aa1eb3154f44edc9f5e9860d48dbad8
MrXlson <dominikdrasar39@gmail.com>
2026-05-29 22:29:05 +0200

    CarpetData.java

--- src/main/java/me/xverse/magiccarpet/CarpetData.java
+++ src/main/java/me/xverse/magiccarpet/CarpetData.java
@@ -1,5 +1,6 @@
 package me.xverse.magiccarpet;
 
+import org.bukkit.Location;
 import org.bukkit.entity.BlockDisplay;
 
 import java.util.List;
@@ -8,11 +9,27 @@ public class CarpetData {
 
     private final List<BlockDisplay> displays;
 
-    public CarpetData(List<BlockDisplay> displays) {
+    private Location center;
+
+    public CarpetData(
+            List<BlockDisplay> displays,
+            Location center
+    ) {
+
         this.displays = displays;
+
+        this.center = center;
     }
 
     public List<BlockDisplay> getDisplays() {
         return displays;
+    }
+
+    public Location getCenter() {
+        return center;
+    }
+
+    public void setCenter(Location center) {
+        this.center = center;
     }
 }
